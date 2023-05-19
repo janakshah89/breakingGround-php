d22b5e388c4ea8751e8c7b7cdf9b9c319442b3fe
rutusha-joshi <rutushakavijoshi@gmail.com>
2023-05-19 17:38:01 +0530

    dd

--- app/Http/Controllers/PropertyMasterController.php
+++ app/Http/Controllers/PropertyMasterController.php
@@ -106,13 +106,13 @@ class PropertyMasterController extends Controller
         try {
             $parameters = $request->all();
             if ($request->hasFile('post_file')) {
-                dd($request->file('post_file')->getPathName());
+                //dd($request->file('post_file'));
                 $extension = $request->file('post_file')->getClientOriginalExtension();
                 if (!in_array($extension, array('xls', 'xlsx', 'csv'))) {
                     return $this->sendBadRequest("not a valid extension");
                 }
-                $path = $request->file('post_file')->getRealPath();
-                $data = Excel::toArray(new PropertyImport(), $path);
+                //$path = $request->file('post_file')->getRealPath();
+                $data = Excel::toArray(new PropertyImport(), $request->file('post_file'));
 
                 return $data;
                 if (!empty($data[0])) {
